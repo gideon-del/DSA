05eeba33ba6b19eba92f16377ac4265cc5beccc5
gideon-del <gideonchidi471@gmail.com>
2023-08-16 13:32:52 +0100

    Done with Array data structur

--- Data Structure/Arrays/main.js
+++ Data Structure/Arrays/main.js
@@ -62,3 +62,48 @@ newArr.push("you");
 newArr.push("!");
 newArr.delete(1);
 console.log(newArr);
+
+function reverese(str) {
+  //check input
+  if (!str || str.length < 2 || typeof str !== "string") {
+    return "Hmm thats not good";
+  }
+  const backwards = [];
+  const totalItems = str.length - 1;
+  for (let i = totalItems; i >= 0; i--) {
+    backwards.push(str[i]);
+  }
+  return backwards.join("");
+}
+function reverse2(str) {}
+console.log(reverese("Hi My Name is Gideon"));
+
+function mergerSortedArrays(array1, array2) {
+  const mergedArray = [];
+  let array1Item = array1[0];
+  let array2Item = array2[0];
+  let i = 1;
+  let j = 1;
+  if (array1.length === 0) {
+    return array2;
+  }
+  if (array2.length === 0) {
+    return array1;
+  }
+  while (array1Item || array2Item) {
+    console.log(array1Item, array2Item);
+    if (!array2Item || array1Item < array2Item) {
+      mergedArray.push(array1Item);
+      array1Item = array1[i];
+      i++;
+    } else {
+      mergedArray.push(array2Item);
+      array2Item = array2[j];
+      j++;
+    }
+  }
+  console.log(mergedArray);
+  return mergedArray;
+}
+
+mergerSortedArrays([1, 3, 4, 5], [1, 2, 3, 4, 5]);
