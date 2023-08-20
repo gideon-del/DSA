be968931807c03d4b4d59320432e03bd58407882
gideon-del <gideonchidi471@gmail.com>
2023-08-20 22:48:28 +0100

    HashTables done

--- Data Structure/HashTable/main.js
+++ Data Structure/HashTable/main.js
@@ -56,4 +56,30 @@ class HashTable {
 const myHashTable = new HashTable(50);
 myHashTable.set("grapes", 1000);
 myHashTable.set("apples", 3000);
-console.log(myHashTable.keys());
+
+//Google Question
+//Given an array = [2,5,1,2,3,5,1,2,4]:
+//It should return 2
+
+//Given an array = [2,1,1,2,3,5,1,2,4]:
+//It should return 1
+
+//Given an array = [2,3,4,5]:
+//It should return undefined
+
+function firstRecurringCharacter(input) {
+  const keys = {};
+  for (let i = 0; i < input.length; i++) {
+    const current = input[i];
+    if (keys[current] !== undefined) {
+      return current;
+    }
+    keys[current] = current;
+  }
+  return undefined;
+}
+
+console.log(firstRecurringCharacter([2, 5, 5, 2, 3, 5, 1, 2, 4]));
+//Bonus... What if we had this:
+// [2,5,5,2,3,5,1,2,4]
+// return 5 because the pairs are before 2,2
