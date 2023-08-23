b43dda33149fb0ed8b731e5d131ad558b7965476
gideon-del <gideonchidi471@gmail.com>
2023-08-23 13:11:02 +0100

    Implemented Linked List append

--- Data Structure/Linked List/main.js
+++ Data Structure/Linked List/main.js
@@ -1,6 +1,47 @@
-let myLinkedList = {
-  head: {
-    value: 4,
-    next: {},
-  },
-};
+// let myLinkedList = {
+//   head: {
+//     value: 10,
+//     next: {
+//       value: 5,
+//       next: {
+//         value: 16,
+//         next: null,
+//       },
+//     },
+//   },
+// };
+
+class LinkedList {
+  constructor(value) {
+    this.head = {
+      value: value,
+      next: null,
+    };
+    this.tail = this.head;
+    this.length = 1;
+  }
+  append(value) {
+    let currentHead = this.head;
+    for (let i = 0; i < this.length; i++) {
+      if (currentHead.next === null) {
+        break;
+      }
+      currentHead = currentHead.next;
+    }
+
+    currentHead.next = {
+      value: value,
+      next: null,
+    };
+    this.tail = {
+      value: value,
+      next: null,
+    };
+    this.length++;
+  }
+}
+
+const myLinkedList = new LinkedList(10);
+myLinkedList.append(5);
+myLinkedList.append(6);
+console.log(myLinkedList);
