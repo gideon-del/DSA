713872f1e9aa8b0be6d8572d1bc39f07c5270ea4
gideon-del <gideonchidi471@gmail.com>
2023-08-27 16:33:42 +0100

    Refactor

--- Data Structure/Stacks_Queues/main.js
+++ Data Structure/Stacks_Queues/main.js
@@ -6,6 +6,9 @@ class Node {
 }
 
 // Stacks
+// LinkedList
+/* 
+
 class Stack {
   constructor() {
     this.top = null;
@@ -25,36 +28,64 @@ class Stack {
       this.append(value);
     } else {
       const newNode = new Node(value);
-      this.top.next = newNode;
+      const holdingPointer = this.top;
+      //   this.top.next = newNode;
       this.top = newNode;
+      this.top.next = holdingPointer;
     }
     this.length++;
     return this;
   }
   pop() {
-    if (!this.bottom) {
+    if (!this.top) {
       return "Stack is Empty";
     }
-    let currentNode = this.bottom;
-    let i = 0;
-    if (this.length === 1) {
+    if (this.top === this.bottom) {
       this.bottom = null;
-      this.top = null;
-    } else {
-      while (i < this.length - 2) {
-        currentNode = currentNode.next;
-        i++;
-      }
-      currentNode.next = null;
-      this.top = currentNode;
     }
+    const holdingPointer = this.top;
+    this.top = this.top.next;
+    let i = 0;
+    // if (this.length === 1) {
+    //   this.bottom = null;
+    //   this.top = null;
+    // } else {
+    //   while (i < this.length - 2) {
+    //     currentNode = currentNode.next;
+    //     i++;
+    //   }
+    //   currentNode.next = null;
+    //   this.top = currentNode;
+    // }
     this.length--;
+    return holdingPointer;
+  }
+}
+*/
+//Array
+class Stack {
+  constructor() {
+    // this.top = null;
+    // this.bottom = null;
+    this.array = [];
+  }
+  peek() {
+    return this.array[this.array.length - 1];
+  }
+  push(value) {
+    this.array.push(value);
+
+    return this;
+  }
+  pop() {
+    this.array.pop();
     return this;
   }
 }
 const myStack = new Stack();
 myStack.push("Google");
 myStack.push("Discord");
+myStack.push("Udemy");
 myStack.pop();
 
 console.log(myStack);
